39241d57fefa24c0cdd59352f294cee9ecd276e0
mattproctor-leake <mattproctorleake@gmail.com>
2020-02-21 14:57:51 +0000

    adding-shake-and-colour

--- etch.js
+++ etch.js
@@ -5,7 +5,7 @@ console.log('it works');
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 20;
 
 // set up canvas for drawing
 
@@ -21,6 +21,8 @@ ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
 // start the drawing 
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
@@ -28,6 +30,9 @@ ctx.stroke();
 
 // write a draw function
 function draw({key}) {
+    //increment hue
+    hue += 5;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 console.log(key);
 // start the path
 ctx.beginPath();
@@ -66,5 +71,16 @@ function handleKey(e) {
     }
 }
 
+// shake function
+
+function clearCanvas () {
+    canvas.classList.add('shake');
+    ctx.clearRect(0, 0, width, height);
+    canvas.addEventListener('animationend', function () {
+        canvas.classList.remove('shake');
+    } ,{once: true} );
+}
+
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
